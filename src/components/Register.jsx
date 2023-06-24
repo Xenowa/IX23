9867cd40672a20612cfd381966dadd74dda33295
Xenowa <xenocodecs@gmail.com>
2023-06-25 05:08:08 +0530

    Fix: Adding preventative measure for form autoscroll to view

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,40 +1,46 @@
-import { Typography, Box } from "@mui/material";
+import { Typography, Box } from "@mui/material"
 import ixbackground from "../assets/IX-bg.jpg"
-import { useContext, useEffect, useState } from "react";
-import useElementVisibility from "../hooks/ElementVisibility";
-import VisibilityContext from "../context/Visibility";
+import { useContext, useEffect, useState } from "react"
+import useElementVisibility from "../hooks/ElementVisibility"
+import VisibilityContext from "../context/Visibility"
 
 export default function Register() {
     // Set visible element context
-    const [isVisible, setIsVisible] = useState();
-    const [containerRef] = useElementVisibility(setIsVisible);
-    const { setVisibleElement } = useContext(VisibilityContext);
-
+    const [isVisible, setIsVisible] = useState()
+    const [containerRef] = useElementVisibility(setIsVisible)
+    const { setVisibleElement } = useContext(VisibilityContext)
+    const [visibilityCounter, setVisibilityCounter] = useState(0)
 
     // =============
     // Tripetto form
     // =============
     useEffect(() => {
-        const tripetto = TripettoServices.init({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiV2QyaytuQjBPd3FpWC92S0tLNmxGYkJwTk01Y21TSnJKZjBzTVNSZDBwbz0iLCJkZWZpbml0aW9uIjoiU01sMW04SWRTMGxuQ3RNREY2aElpRTNpb1lnTGNYT2RpT3lPc0RxY3JBcz0iLCJ0eXBlIjoiY29sbGVjdCJ9.WzeVrXepj7OqmqrdgtsTPUExAw1IRlocjVuxJt5lcXI" });
+        if (visibilityCounter === 1) {
+            const tripetto = TripettoServices.init({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiV2QyaytuQjBPd3FpWC92S0tLNmxGYkJwTk01Y21TSnJKZjBzTVNSZDBwbz0iLCJkZWZpbml0aW9uIjoiU01sMW04SWRTMGxuQ3RNREY2aElpRTNpb1lnTGNYT2RpT3lPc0RxY3JBcz0iLCJ0eXBlIjoiY29sbGVjdCJ9.WzeVrXepj7OqmqrdgtsTPUExAw1IRlocjVuxJt5lcXI" });
 
-        TripettoChat.run({
-            element: document.getElementById("tripetto"),
-            definition: tripetto.definition,
-            styles: tripetto.styles,
-            display: "inline",
-            l10n: tripetto.l10n,
-            locale: tripetto.locale,
-            translations: tripetto.translations,
-            attachments: tripetto.attachments,
-            onSubmit: tripetto.onSubmit,
-            snapshot: tripetto.snapshot,
-            onPause: tripetto.onPause,
-            persistent: true,
-        });
-    }, [])
+            TripettoChat.run({
+                element: document.getElementById("tripetto"),
+                definition: tripetto.definition,
+                styles: tripetto.styles,
+                display: "inline",
+                l10n: tripetto.l10n,
+                locale: tripetto.locale,
+                translations: tripetto.translations,
+                attachments: tripetto.attachments,
+                onSubmit: tripetto.onSubmit,
+                snapshot: tripetto.snapshot,
+                onPause: tripetto.onPause,
+                persistent: true
+            })
+        }
+    }, [visibilityCounter])
+
 
     useEffect(() => {
-        if (isVisible) setVisibleElement("register");
+        if (isVisible) {
+            setVisibleElement("register")
+            setVisibilityCounter(visibilityCounter + 1)
+        }
     }, [isVisible])
 
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-// Loading <Fonts></Fonts>
+// Loading Fonts
 import "@fontsource/poppins"
 
 // Creating a custom theme
