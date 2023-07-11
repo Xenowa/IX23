ac6ffe1d9bf71c931457f899e9e22abd38af49f5
Adeepa Gunathilake <adeepagunathilake@gmail.com>
2023-07-11 12:15:11 +0530

    add social media buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Link, Typography } from "@mui/material";
 // import AquathonLightIcon from "../assets/Auquadrop-light.png"
 import IXIcon from "../assets/IX.svg"
 import ReactParticles from "./ReactParticles";
@@ -6,6 +6,7 @@ import { useContext, useEffect, useState } from "react";
 import useElementVisibility from "../hooks/ElementVisibility";
 import VisibilityContext from "../context/Visibility";
 import zIndex from "@mui/material/styles/zIndex";
+import { Facebook, Instagram, LinkedIn } from "@mui/icons-material";
 
 export default function Home() {
     const [matches, setMatches] = useState(
@@ -65,12 +66,29 @@ export default function Home() {
                 textAlign="center"
                 sx={{
                     fontStyle: "italic",
-                    marginBottom: "3rem",
+                    marginBottom: "1rem",
                     zIndex: "2"
                 }}
             >
                 "Unravelling Visions beyond Tradition"
             </Typography>
+            <Box sx={{
+                position: "relative",
+                zIndex: "2",
+                marginBottom: "1rem",
+                display: "flex",
+                gap: "15px",
+            }}>
+                <Link color="inherit" underline="none" href="https://www.instagram.com/designix.iit" target="_blank" sx={{ display: "flex" }}>
+                    <Instagram sx={{ color: "primary.main" }} fontSize="medium" />
+                </Link>
+                <Link color="inherit" underline="none" href="https://web.facebook.com/people/IX-23/100093309528840/" target="_blank" sx={{ display: "flex" }}>
+                    <Facebook sx={{ color: "primary.main" }} fontSize="medium" />
+                </Link>
+                <Link color="inherit" underline="none" href="https://www.linkedin.com/company/ix-23/" target="_blank" sx={{ display: "flex" }}>
+                    <LinkedIn sx={{ color: "primary.main" }} fontSize="medium" />
+                </Link>
+            </Box>
             <Button
                 component="a"
                 sx={{ 
