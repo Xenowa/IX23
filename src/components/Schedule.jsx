f0073c27c671ddb286a14e37d2a57a46b1573eca
Adeepa Gunathilake <adeepagunathilake@gmail.com>
2023-07-16 11:33:10 +0530

    extracted timeline events into a seperate component

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -30,198 +30,101 @@ export default function Schedule() {
         if (isVisible) setVisibleElement("schedule");
     }, [isVisible])
 
+    const TimelineEvent = ({heading, title, date, icon}) => (
+        <TimelineItem>
+            <TimelineOppositeContent sx={{ m: 'auto 0' }}>
+                <Typography variant={heading} component="span" fontWeight="bold" sx={{ wordBreak: "break-word" }}>
+                    { title }
+                </Typography>
+                <Typography variant="h6" component="h6" sx={{ wordBreak: "break-word" }}>
+                    { date }
+                </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+                <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
+                <TimelineDot color="secondaryLight" variant="filled">
+                    { icon }
+                </TimelineDot>
+                <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
+            </TimelineSeparator>
+            <TimelineContent sx={{ m: 'auto 0' }} />
+        </TimelineItem>
+    )
+
     const Content = ({ heading }) => {
+        const iconSxProp = { color: "primary.main", padding: "1rem", borderRadius: "20rem", border: "1px solid var(--primary)" }
+
         return (
             <div>
-                <TimelineItem>
-                    <TimelineOppositeContent sx={{ m: 'auto 0' }}>
-                        <Typography variant={heading} component="span" fontWeight="bold" sx={{ wordBreak: "break-word" }}>
-                            Opening of Registrations
-                        </Typography>
-                        <Typography variant="h6" component="h6" sx={{ wordBreak: "break-word" }}>
-                            July 12th
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                        <TimelineDot color="secondaryLight" variant="filled">
-                            <ScheduleSendIcon sx={{ color: "primary.main", padding: "1rem", borderRadius: "20rem", border: "1px solid var(--primary)" }} fontSize="medium" />
-                        </TimelineDot>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ m: 'auto 0' }} />
-                </TimelineItem>
+                <TimelineEvent
+                    heading={heading}
+                    title={"Opening of Registrations"}
+                    date={"July 12th"}
+                    icon={<ScheduleSendIcon sx={iconSxProp} fontSize="medium" />}
+                />
 
-                <TimelineItem>
-                    <TimelineOppositeContent sx={{ m: 'auto 0' }}>
-                        <Typography variant={heading} component="span" fontWeight="bold" sx={{ wordBreak: "break-word" }}>
-                            Closing of Registrations
-                        </Typography>
-                        <Typography variant="h6" component="h6" sx={{ wordBreak: "break-word" }}>
-                            July 22nd
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                        <TimelineDot color="secondaryLight" variant="filled">
-                            <CancelScheduleSendIcon sx={{ color: "primary.main", padding: "1rem", borderRadius: "20rem", border: "1px solid var(--primary)" }} fontSize="medium" />
-                        </TimelineDot>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ m: 'auto 0' }} />
-                </TimelineItem>
+                <TimelineEvent
+                    heading={heading}
+                    title={"Closing of Registrations"}
+                    date={"July 22nd"}
+                    icon={<CancelScheduleSendIcon sx={iconSxProp} fontSize="medium" />}
+                />
 
-                <TimelineItem>
-                    <TimelineOppositeContent sx={{ m: 'auto 0' }}>
-                        <Typography variant={heading} component="span" fontWeight="bold" sx={{ wordBreak: "break-word" }}>
-                            Awarness Session
-                        </Typography>
-                        <Typography variant="h6" component="h6" sx={{ wordBreak: "break-word" }}>
-                            July 25th
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                        <TimelineDot color="secondaryLight" variant="filled">
-                            <CoPresentIcon sx={{ color: "primary.main", padding: "1rem", borderRadius: "20rem", border: "1px solid var(--primary)" }} fontSize="medium" />
-                        </TimelineDot>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ m: 'auto 0' }} />
-                </TimelineItem>
+                <TimelineEvent
+                    heading={heading}
+                    title={"Awareness Session"}
+                    date={"July 25th"}
+                    icon={<CoPresentIcon sx={iconSxProp} fontSize="medium" />}
+                />
 
-                <TimelineItem>
-                    <TimelineOppositeContent sx={{ m: 'auto 0' }}>
-                        <Typography variant={heading} component="span" fontWeight="bold" sx={{ wordBreak: "break-word" }}>
-                            Initial Design Submission Opening
-                        </Typography>
-                        <Typography variant="h6" component="h6" sx={{ wordBreak: "break-word" }}>
-                            July 26th
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                        <TimelineDot color="secondaryLight" variant="filled">
-                            <EditIcon sx={{ color: "primary.main", padding: "1rem", borderRadius: "20rem", border: "1px solid var(--primary)" }} fontSize="medium" />
-                        </TimelineDot>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ m: 'auto 0' }} />
-                </TimelineItem>
+                <TimelineEvent
+                    heading={heading}
+                    title={"Initial Design Submission Opening"}
+                    date={"July 26th"}
+                    icon={<EditIcon sx={iconSxProp} fontSize="medium" />}
+                />
 
-                <TimelineItem>
-                    <TimelineOppositeContent sx={{ m: 'auto 0' }}>
-                        <Typography variant={heading} component="span" fontWeight="bold" sx={{ wordBreak: "break-word" }}>
-                            Workshop 1
-                        </Typography>
-                        <Typography variant="h6" component="h6" sx={{ wordBreak: "break-word" }}>
-                            July 29th
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                        <TimelineDot color="secondaryLight" variant="filled">
-                            <HandymanIcon sx={{ color: "primary.main", padding: "1rem", borderRadius: "20rem", border: "1px solid var(--primary)" }} fontSize="medium" />
-                        </TimelineDot>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ m: 'auto 0' }} />
-                </TimelineItem>
+                <TimelineEvent
+                    heading={heading}
+                    title={"Workshop 1"}
+                    date={"July 29th"}
+                    icon={<HandymanIcon sx={iconSxProp} fontSize="medium" />}
+                />
 
-                <TimelineItem>
-                    <TimelineOppositeContent sx={{ m: 'auto 0' }}>
-                        <Typography variant={heading} component="span" fontWeight="bold" sx={{ wordBreak: "break-word" }}>
-                            Initial Design Submission Closing
-                        </Typography>
-                        <Typography variant="h6" component="h6" sx={{ wordBreak: "break-word" }}>
-                            Aug 2nd
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                        <TimelineDot color="secondaryLight" variant="filled">
-                            <EditOffIcon sx={{ color: "primary.main", padding: "1rem", borderRadius: "20rem", border: "1px solid var(--primary)" }} fontSize="medium" />
-                        </TimelineDot>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ m: 'auto 0' }} />
-                </TimelineItem>
+                <TimelineEvent
+                    heading={heading}
+                    title={"Initial Design Submission Closing"}
+                    date={"Aug 2nd"}
+                    icon={<EditOffIcon sx={iconSxProp} fontSize="medium" />}
+                />
 
-                <TimelineItem>
-                    <TimelineOppositeContent sx={{ m: 'auto 0' }}>
-                        <Typography variant={heading} component="span" fontWeight="bold" sx={{ wordBreak: "break-word" }}>
-                            Workshop 2
-                        </Typography>
-                        <Typography variant="h6" component="h6" sx={{ wordBreak: "break-word" }}>
-                            Aug 9th
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                        <TimelineDot color="secondaryLight" variant="filled">
-                            <HandymanIcon sx={{ color: "primary.main", padding: "1rem", borderRadius: "20rem", border: "1px solid var(--primary)" }} fontSize="medium" />
-                        </TimelineDot>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ m: 'auto 0' }} />
-                </TimelineItem>
+                <TimelineEvent
+                    heading={heading}
+                    title={"Workshop 2"}
+                    date={"Aug 9th"}
+                    icon={<HandymanIcon sx={iconSxProp} fontSize="medium" />}
+                />
 
-                <TimelineItem>
-                    <TimelineOppositeContent sx={{ m: 'auto 0' }}>
-                        <Typography variant={heading} component="span" fontWeight="bold" sx={{ wordBreak: "break-word" }}>
-                            Announcement of Finalists
-                        </Typography>
-                        <Typography variant="h6" component="h6" sx={{ wordBreak: "break-word" }}>
-                            Aug 13th
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                        <TimelineDot color="secondaryLight" variant="filled">
-                            <AutoAwesomeIcon sx={{ color: "primary.main", padding: "1rem", borderRadius: "20rem", border: "1px solid var(--primary)" }} fontSize="medium" />
-                        </TimelineDot>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ m: 'auto 0' }} />
-                </TimelineItem>
+                <TimelineEvent
+                    heading={heading}
+                    title={"Announcement of Finalists"}
+                    date={"Aug 13th"}
+                    icon={<AutoAwesomeIcon sx={iconSxProp} fontSize="medium" />}
+                />
 
-                <TimelineItem>
-                    <TimelineOppositeContent sx={{ m: 'auto 0' }}>
-                        <Typography variant={heading} component="span" fontWeight="bold" sx={{ wordBreak: "break-word" }}>
-                            Designathon
-                        </Typography>
-                        <Typography variant="h6" component="h6" sx={{ wordBreak: "break-word" }}>
-                            Aug 19th
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                        <TimelineDot color="secondaryLight" variant="filled">
-                            <DrawIcon sx={{ color: "primary.main", padding: "1rem", borderRadius: "20rem", border: "1px solid var(--primary)" }} fontSize="medium" />
-                        </TimelineDot>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ m: 'auto 0' }} />
-                </TimelineItem>
+                <TimelineEvent
+                    heading={heading}
+                    title={"Designathon"}
+                    date={"Aug 19th"}
+                    icon={<DrawIcon sx={iconSxProp} fontSize="medium" />}
+                />
 
-                <TimelineItem>
-                    <TimelineOppositeContent sx={{ m: 'auto 0' }}>
-                        <Typography variant={heading} component="span" fontWeight="bold" sx={{ wordBreak: "break-word" }}>
-                            Grab the Trophy
-                        </Typography>
-                        <Typography variant="h6" component="h6" sx={{ wordBreak: "break-word" }}>
-                            Aug 20th
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                        <TimelineDot color="secondaryLight" variant="filled">
-                            <EmojiEventsIcon sx={{ color: "primary.main", padding: "1rem", borderRadius: "20rem", border: "1px solid var(--primary)" }} fontSize="medium" />
-                        </TimelineDot>
-                        <TimelineConnector sx={{ bgcolor: 'primary.main', height: "2rem" }} />
-                    </TimelineSeparator>
-                    <TimelineContent sx={{ m: 'auto 0' }} />
-                </TimelineItem>
+                <TimelineEvent
+                    heading={heading}
+                    title={"Grab the Trophy"}
+                    date={"Aug 20th"}
+                    icon={<EmojiEventsIcon sx={iconSxProp} fontSize="medium" />}
+                />
             </div>
         )
     }
