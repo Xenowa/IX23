736734942ca68ca5dc77c5cf89e09455a4d7b9a5
Xenowa <xenocodecs@gmail.com>
2023-08-24 19:48:46 +0530

    Adding gallery redirects

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Errorpage from './pages/Errorpage'
 import Finalepage from './pages/Finalepage'
+import Redirectpage from './pages/Redirectpage'
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/finale" element={<Finalepage />} />
+        {/* Create a redirect to youtube on route /finale2 */}
+        <Route path="/gallery-1" element={<Redirectpage url="https://m.facebook.com/story.php?story_fbid=pfbid0BffZGkTFSLLPXta7TdDxYTBk1dvswZzJ7rRebi82KmiKXuVTdycuVDSCVNtCaxDCl&id=100093309528840&mibextid=Nif5oz" />} />
+        <Route path="/gallery-2" element={<Redirectpage url="https://m.facebook.com/story.php?story_fbid=pfbid02Biiq5y3gACL56aWsgyBEjqQM42YgvEe5g1zHof9mrDzUNCx9iWPrBV72ignNe4kxl&id=100093309528840&mibextid=Nif5oz" />} />
         <Route path="/*" element={<Errorpage />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Redirectpage.jsx
@@ -0,0 +1,8 @@
+export default function Redirectpage({ url }) {
+    // redirect to external website on landing
+    window.location.href = url
+
+    return (
+        <div />
+    )
+}
